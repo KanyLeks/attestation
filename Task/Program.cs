2d2d6296588e84f5474f51a7c7461dc8b1ae8a13
Aleksey Kanaev <my@akanaev.ru>
2023-03-29 20:17:50 +0300

    Задача

---
+++ Task/Program.cs
@@ -0,0 +1,24 @@
+﻿string[] arrayOne = new string[5] { "757", "23", "ZiDan", "Metallica", "ROW" };
+string[] arrayTwo = new string[arrayOne.Length];
+void SecondArray(string[] arrayOne, string[] arrayTwo)
+{
+    int count = 0;
+    for (int i = 0; i < arrayOne.Length; i++)
+    {
+        if (arrayOne[i].Length <= 3)
+        {
+            arrayTwo[count] = arrayOne[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArray(arrayOne, arrayTwo);
+PrintArray(arrayTwo);
